8337212f51f08c3e47482623566f2a2d27ead1f9
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-02-12 12:26:23 -0300

    Fixed SSL listening on 80.

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -17,10 +17,26 @@ node['mo_server_reverse_proxy_cache']['virtual_host'].each do |vhost|
             'proxy_pass' => "http://#{vhost[0]}_#{app[:name]}"
           }
         })
-      if app[:ssl]
+      notifies :reload, "service[nginx]"
+    end
+    if app[:ssl]
+      nginx_conf_file "#{vhost[0]}_#{app[:name]}_ssl" do
+        listen app[:listen_ssl] || "443"
+        upstream "#{vhost[0]}_#{app[:name]}_ssl" => {"server" => "#{node['varnish']['listen_address']}:#{node['varnish']['listen_port']}"}
+        server_name app[:server_name]
+        locations({
+            "/" => {
+              "proxy_set_header" => {
+                "X-Forwarded-For" => "$proxy_add_x_forwarded_for",
+                "Host" => "$http_host"
+              },
+              'proxy_redirect' => 'off',
+              'proxy_pass' => "http://#{vhost[0]}_#{app[:name]}_ssl"
+            }
+          })
+        notifies :reload, "service[nginx]"
         ssl ssl_for(app)
       end
-      notifies :reload, "service[nginx]"
     end
   end
 end
